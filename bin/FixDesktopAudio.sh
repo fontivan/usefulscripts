5e4f210bf13ca692079ff54e97f2ade53c40b7d8
Steven Skeard <stevenskeard@gmail.com>
2021-11-19 19:47:14 -0500

    Update amixer card number

--- bin/FixDesktopAudio.sh
+++ bin/FixDesktopAudio.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Set gain for both inputs to 0% (0dB)
-amixer -c0 set 'Line Boost',0 0
-amixer -c0 set 'Rear Mic Boost',0 0
+amixer -c1 set 'Line Boost',0 0
+amixer -c1 set 'Rear Mic Boost',0 0
 
-# Restart pulseaudio
-pulseaudio -k && pulseaudio --start
+# Restart pipewire
+systemctl --user restart pipewire.service
 
